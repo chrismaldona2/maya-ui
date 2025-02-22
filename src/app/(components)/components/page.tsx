96ceea2ca2efdc774dff4e3fc652ec74548dccbc
Christian Maldonado <chris.ariel.maldonado@gmail.com>
2025-02-22 12:15:43 -0300

    struggling with this one component that doesnt want to be imported on vercel

--- src/app/(components)/components/page.tsx
+++ src/app/(components)/components/page.tsx
@@ -3,23 +3,6 @@ import ScrollToTop from "@/components/layout/scroll-to-top";
 import { allDocs } from "contentlayer/generated";
 import { getMDXComponent } from "next-contentlayer2/hooks";
 
-const UiVerseLink = () => {
-  return (
-    <a
-      className="font-semibold bg-gradient-to-br from-[#8689f3] to-[#945abe] to-70% bg-clip-text text-transparent"
-      href="https://uiverse.io/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      UIVerse
-    </a>
-  );
-};
-
-const components = {
-  UiVerseLink,
-};
-
 const ComponentsPage = () => {
   const doc = allDocs.find((doc) => doc._raw.flattenedPath === "introduction");
 
@@ -36,10 +19,23 @@ const ComponentsPage = () => {
         {doc.title}
       </h1>
       <div className="text-lg text-neutral-500 dark:text-neutral-400 flex flex-col gap-4">
-        <Content components={components} />
+        <Content components={{ UiVerseLink }} />
       </div>
     </>
   );
 };
 
 export default ComponentsPage;
+
+export const UiVerseLink = () => {
+  return (
+    <a
+      className="font-semibold bg-gradient-to-br from-[#8689f3] to-[#945abe] to-70% bg-clip-text text-transparent"
+      href="https://uiverse.io/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      UIVerse
+    </a>
+  );
+};
